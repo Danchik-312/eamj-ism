190f30e401c892e928382fd115c4ab9a68817685
Danil Karpushevich <noname211070@gmail.com>
2025-06-07 19:38:43 +0600

    add side bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import MainPage from "./pages/mainPage/MainPage";
+import AnnouncementsPage from "./pages/announcementsPage/AnnouncementsPage";
+import ArchivesPage from "./pages/archivesPage/ArchivesPage";
+import ContactPage from "./pages/contactPage/ContactPage";
+import CurrentPage from "./pages/currentPage/CurrentPage";
+import EditorialPage from "./pages/editorialPage/EditorialPage";
+import HomePage from "./pages/homePage/HomePage";
+import IndexingPage from "./pages/indexingPage/IndexingPage";
+import PublicationPage from "./pages/publicationPage/PublicationPage";
+import SubmissionsPage from "./pages/submissionsPage/SubmissionsPage";
+import NotFoundPage from "./pages/notFoundPage/NotFoundPage";
+import SideBar from "./components/sideBar/SideBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Header />
+          <div  className="_container flex">
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/announcements" element={<AnnouncementsPage />} />
+                <Route path="/archives" element={<ArchivesPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/current" element={<CurrentPage />} />
+                <Route path="/editorial" element={<EditorialPage />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/indexing" element={<IndexingPage />} />
+                <Route path="/publication" element={<PublicationPage />} />
+                <Route path="/submissions" element={<SubmissionsPage />} />
+                <Route path='*' element={<NotFoundPage/>} />
+            </Routes>
+          <div>
+               <SideBar />
+          </div>
+          </div>
+          <Footer />
+      </BrowserRouter>
     </div>
   );
 }
